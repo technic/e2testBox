e5c3f0fa2f7022fb756912455b58140c90fe4550
Alex Maystrenko <alexeytech@gmail.com>
2018-01-19 22:54:59 +0100

    vm: add opkg build, install and configuration

--- provision/build.sh
+++ provision/build.sh
@@ -69,6 +69,46 @@ make
 make install
 cd -
 
+
+sudo apt-get install -y libarchive-dev  libcurl4-openssl-dev libgpgme11-dev
+
+opkgVer="0.3.5"
+opkgDir="opkg-$opkgVer"
+if ! test -d "$opkgDir"; then
+    wget "http://git.yoctoproject.org/cgit/cgit.cgi/opkg/snapshot/opkg-$opkgVer.tar.gz"
+    tar -xzf "$opkgDir".tar.gz
+fi
+cd "$opkgDir"
+./autogen.sh
+./configure --enable-curl --enable-ssl-curl --enable-gpg
+make
+sudo checkinstall -y --pkgversion=$opkgVer
+cd -
+
+sudo install -D -m 644 /vagrant/provision/opkg.conf /usr/local/etc/opkg/opkg.conf
+
+cd ~/src
+mkdir -p fakepkg
+mkdir -p fakepkg/DEBIAN
+
+cat > "fakepkg/DEBIAN/control" <<EOF
+Package: enigma2
+Version: 1.0
+Depends:
+Description: enigma2 fake package
+Section: extra
+Priority: optional
+Maintainer: unknown
+License: unknown
+Architecture: all
+EOF
+
+dpkg-deb -b fakepkg enigma2.deb
+# install enigma2 fake package to make dependent packages happy
+opkg install enigma2.deb
+cd -
+
+
 if ! test -d skin-PLiHD-master; then
     wget https://github.com/littlesat/skin-PLiHD/archive/master.zip -O skin-PLiHD-master.zip
     unzip skin-PLiHD-master.zip
